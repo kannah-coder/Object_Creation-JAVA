fa81cd337aa58168e5c14e5d67f0dc9f6e948911
kannah-coder <lingabadrirohit@gmail.com>
2025-10-24 21:43:34 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,13 +1,35 @@
 class Student {
     String name;
-    Student(String n) {
-        name = n;
+    int marks;
+    String section;
+    void displayInfo(int studingIn){
+        System.out.println("Name of the student:"+name);
+        System.out.println("Marks of the student:"+marks);
+        System.out.println("Section:"+section); 
+        System.out.println("classs:"+studingIn);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student("Kannah"); // regular object creation
-        System.out.println(s1.name);
+        Student s1 = new Student(); // regular object creation
+        s1.name="kannah";
+        s1.marks=100;
+        s1.section="A";
+        s1.displayInfo(12);
+        //display detailts 
+
+        Student s2 = new Student(); // Another object creation
+        s2.name="Peter";
+        s2.marks=64;
+        s2.section="A";
+        s2.displayInfo(12);
+
+
+        Student s3 = new Student(); // Another object creation
+        s3.name="Mikey";
+        s3.marks=76;
+        s3.section="B";
+        s3.displayInfo(11);
     }
 }
